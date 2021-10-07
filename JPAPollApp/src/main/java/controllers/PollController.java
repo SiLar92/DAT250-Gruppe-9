b3d16f8ce43499e52b20e327044e3921362fb748
magnus <578031@hvl.stud.no>
2021-10-07 15:39:24 +0200

    changed getPoll method

--- JPAPollApp/src/main/java/controllers/PollController.java
+++ JPAPollApp/src/main/java/controllers/PollController.java
@@ -2,25 +2,39 @@ package controllers;
 
 import DAO.PollDAO;
 import models.Poll;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path="/poll")
 public class PollController {
 
 
-    @GetMapping("/polls")
+    @GetMapping("")
     public List<Poll> getPolls() {
         return new PollDAO().getAll();
     }
-
-    @GetMapping("/poll")
+    // /poll?id=3
+   /* @GetMapping("/poll")
     public Poll getPoll(@RequestParam(value = "id") Long id) {
         return new PollDAO().findById(id);
+    }*/
+
+
+    /**
+     * returns specific polls by id
+     * example where id is 3:  /poll/3
+     * @param id of poll
+     * @return poll by id
+     */
+    @GetMapping("/{id}")
+    public Poll getById(@PathVariable Long id) {
+        return new PollDAO().findById(id);
     }
+
+
+
 
 
 }
